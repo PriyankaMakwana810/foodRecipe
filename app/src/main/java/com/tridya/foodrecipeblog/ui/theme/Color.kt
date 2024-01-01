96df1bb20bb10bfea42747d32f76c95a4bb2688f
Priyanka <priyankamakwana8002@gmail.com>
2024-01-01 14:34:10 +0530

    init commit

---
+++ app/src/main/java/com/tridya/foodrecipeblog/ui/theme/Color.kt
@@ -0,0 +1,44 @@
+package com.tridya.foodrecipeblog.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+//Primary
+val primary100 = Color(0xFF129575)
+val primary80 = Color(0xFF71B1A1)
+val primary60 = Color(0xFFAFD3CA)
+val primary40 = Color(0xFFDBEBE7)
+val primary20 = Color(0xFFF6FAF9)
+
+//Secondary
+val secondary100 = Color(0xFFFF9C00)
+val secondary80 = Color(0xFFFFA61A)
+val secondary60 = Color(0xFFFFBA4D)
+val secondary40 = Color(0xFFFFCE80)
+val secondary20 = Color(0xFFFFE1B3)
+
+//Netural Colour
+val black = Color(0xFF000000)
+val gray1 = Color(0xFF484848)
+val gray2 = Color(0xFF797979)
+val gray3 = Color(0xFFA9A9A9)
+val gray4 = Color(0xFFD9D9D9)
+val white = Color(0xFFFFFFFF)
+
+val rating = Color(0xFFFFAD30)
+
+//Warning
+val Warning1 = Color(0xFFFD3654)
+val Warning2 = Color(0xFFFFE1E7)
+
+//Green
+val success = Color(0xFF31B057)
+
+val transparent = Color(0)
