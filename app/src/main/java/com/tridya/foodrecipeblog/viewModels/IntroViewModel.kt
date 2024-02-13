8a440f9cf7226db3344c457b600bdb73cfc4e871
Priyanka <priyankamakwana8002@gmail.com>
2024-02-13 13:00:35 +0530

    - code cleanup

--- app/src/main/java/com/tridya/foodrecipeblog/viewModels/IntroViewModel.kt
+++ app/src/main/java/com/tridya/foodrecipeblog/viewModels/IntroViewModel.kt
@@ -1,15 +1,10 @@
 package com.tridya.foodrecipeblog.viewModels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.tridya.foodrecipeblog.api.repo.CommonRepository
-import com.tridya.foodrecipeblog.database.tables.RecipeCard
 import com.tridya.foodrecipeblog.utils.Constants
 import com.tridya.foodrecipeblog.utils.PrefUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 
